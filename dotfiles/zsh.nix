ba30f861b9fd81b9ffe2082740dc8b7290d11d20
blue <blue@nixos.local>
2026-09-13 23:13:40 +0700

    auto: 2026-09-13_23:13:40

--- dotfiles/zsh.nix
+++ dotfiles/zsh.nix
@@ -33,8 +33,8 @@
 
       nrs() {
         cd /etc/nixos &&
-        sudo git add -A &&
-        sudo git commit -m "auto: $(date +%Y-%m-%d_%H:%M:%S)" --allow-empty &&
+        git add -A &&
+        git commit -m "auto: $(date +%Y-%m-%d_%H:%M:%S)" --allow-empty &&
         sudo nixos-rebuild switch --flake .#main-nixos &&
         git push 
         }
