a38160b3735a574da5480652707e939ee76eebe2
blue <blue@nixos.local>
2026-09-13 23:04:29 +0700

    auto: 2026-09-13_23:04:29

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
+  }
 
   outputs = { self, nixpkgs, home-manager, ... }:
   let
