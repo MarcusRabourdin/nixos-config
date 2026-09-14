ea1a97cbd2464b350d47f5b645df61f7b2f80d9f
marcus <marcus.rabourdin@epita.fr>
2026-09-14 17:22:34 +0700

    nrs: 2026-22-14_17:22:34

--- dotfiles/zsh.nix
+++ dotfiles/zsh.nix
@@ -25,17 +25,17 @@
       test = "print test";
     };
 
-    shellInit = ''
-        autoload -Uz vcs_info
-        precmd() { vcs_info }
-
-        zstyle ':vcs_info:git:*' formats ' (%b)'
-        PROMPT='%F{cyan}%n@%m%f:%F{blue}%~%f%F{yellow}$vcs_info_msg_0_%f %# '
-      '';
 
     initContent = ''
-      export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=8"
 
+      
+      autoload -Uz vcs_info
+      precmd() { vcs_info }
+      zstyle ':vcs_info:git:*' formats ' (%b)'
+      PROMPT='%F{cyan}%n@%m%f:%F{blue}%~%f%F{yellow}$vcs_info_msg_0_%f %# '
+
+      
+      export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=8"
       export LESS_TERMCAP_mb=$'\e[1;32m'
       export LESS_TERMCAP_md=$'\e[1;34m'
       export LESS_TERMCAP_me=$'\e[0m'
