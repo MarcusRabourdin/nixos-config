2215e8a05771708ca5dfde36558424ee6bcbe296
marcus <marcus.rabourdin@epita.fr>
2026-09-14 17:24:20 +0700

    nrs: 2026-24-14_17:24:20

--- dotfiles/zsh.nix
+++ dotfiles/zsh.nix
@@ -32,6 +32,7 @@
       autoload -Uz vcs_info
       precmd() { vcs_info }
       zstyle ':vcs_info:git:*' formats ' (%b)'
+      setopt PROMPT_SUBST
       PROMPT='%F{cyan}%n@%m%f:%F{blue}%~%f%F{yellow}$vcs_info_msg_0_%f %# '
 
       
