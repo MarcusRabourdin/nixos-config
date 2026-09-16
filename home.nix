a6585f8aae0940c2733dd0f596c03bb3e637ef7e
marcus <marcus.rabourdin@epita.fr>
2026-09-16 16:17:38 +0700

    nrs: 2026-17-16_16:17:38

--- home.nix
+++ home.nix
@@ -40,6 +40,18 @@
     };
   };
 
+  # ============================== PipreWire ==============================#
+
+  sound.enable = false;
+
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enalbe = true;
+  }
     
   # ============================== Dotfiles ============================== #
 
